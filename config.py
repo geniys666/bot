cebcd58f5e6d9d9cdc51a896b7a2f4033f706ed3
geniys666 <130176169+geniys666@users.noreply.github.com>
2023-07-02 20:04:17 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+TOKEN = "5999047242:AAGh1o9R-lwVlA4ZPWoIo7quRI-SmiSH_Fc"
+
+keys = {
+    'Биткоин': 'BTC',
+    'Эфириум' : 'ETH',
+    'Доллар' : 'USD',
+    'Евро' : 'EUR',
+    'Рубль' : 'RUB',
+    'Лира' : 'TRY'
+}
